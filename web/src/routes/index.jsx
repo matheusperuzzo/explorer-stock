b519742252b1ee626efb254cf788434021dffae2
Matheus Peruzzo <matheusperuzzodev@gmail.com>
2024-10-26 23:41:49 -0400

    Add authorization functions

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,30 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { AdminRoutes } from "./admin.routes";
+import { AuthRoutes } from "./auth.routes";
+import { CustomerRoutes } from "./customer.routes";
+import { SalesRoutes } from "./sales.routes";
+import { USER_ROLE } from "../utils/roles";
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoutes() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.SALES:
+        return <SalesRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
+
   return (
-    <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
-    </BrowserRouter>
+    <BrowserRouter>{user ? <AccessRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 }
---
+++ web/src/utils/roles.js
@@ -0,0 +1,5 @@
+export const USER_ROLE = {
+  ADMIN: "admin",
+  SALES: "sales",
+  CUSTOMER: "customer",
+};
